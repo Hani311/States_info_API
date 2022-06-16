ea4ad3f0fea9b9f759fb6207f4dfbba7e9568168
Hani <57943681+Hani311@users.noreply.github.com>
2022-06-16 20:28:22 +0200

    initial commit

---
+++ app/src/main/java/com/example/states_info_api/utils/NetworkUtils.java
@@ -0,0 +1,30 @@
+package com.example.states_info_api.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    public static String makeHTTPRequest(URL url) throws IOException {
+        HttpURLConnection connection =(HttpURLConnection) url.openConnection();
+        InputStream inputStream = connection.getInputStream();
+
+        try {
+
+            Scanner scanner = new Scanner(inputStream);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput)
+                return scanner.next();
+            else return null;
+        }
+        finally {
+            connection.disconnect();
+        }
+
+    }
+}
